3c3331dd32374079b6cace0799466eba374bd9f3
Ashish Kumar <47529525+ashishkumar5947@users.noreply.github.com>
2026-08-07 01:58:24 +0530

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.java
@@ -0,0 +1,41 @@
+class Solution {
+    // BRUTE
+    // public boolean isAnagram(String s, String t) {
+    //     if (s.length() != t.length()) {
+    //         return false;
+    //     }
+
+    //     char[] word1 = s.toCharArray();
+    //     char[] word2 = t.toCharArray();
+
+    //     Arrays.sort(word1);
+    //     Arrays.sort(word2);
+
+    //     return Arrays.equals(word1, word2);
+    // }
+
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length())
+            return false;
+
+        Map<Character, Integer> frequency = new HashMap<>();
+        for (char c : s.toCharArray()) {
+            frequency.put(c, frequency.getOrDefault(c, 0) + 1);
+        }
+
+        for (char c : t.toCharArray()) {
+            if (!frequency.containsKey(c))
+                return false;
+
+            frequency.put(c, frequency.get(c) - 1);
+
+            if (frequency.get(c) < 0)
+                return false;
+
+            if (frequency.get(c) == 0)
+                frequency.remove(c);
+        }
+
+        return frequency.isEmpty();
+    }
+}
