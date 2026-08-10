db62843ea2f3deac71cb293a9914dcbba4da334e
Ashish Kumar <47529525+ashishkumar5947@users.noreply.github.com>
2026-08-11 00:43:13 +0530

    Add: longest-common-prefix - submission-2

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-2.java
@@ -0,0 +1,55 @@
+class Solution {
+    // public String longestCommonPrefix(String[] strs) {
+    //     int n = strs.length;
+    //     String prefix = strs[0];
+
+    //     for (int i = 1; i < n; i++) {
+    //         String s1 = strs[i];
+    //         prefix = getLongestPrefix(s1, prefix);
+    //     }
+
+    //     return prefix;
+    // }
+
+    // private String getLongestPrefix(String s1, String s2) {
+    //     StringBuilder prefix = new StringBuilder();
+    //     int i = 0, j = 0;
+
+    //     while (i < s1.length() && j < s2.length()) {
+    //         if (s1.charAt(i) != s2.charAt(j))
+    //             return prefix.toString();
+
+    //         prefix.append(s1.charAt(i));
+    //         i++;
+    //         j++;
+    //     }
+
+    //     return prefix.toString();
+    // }
+
+    // OPTIMIZED
+    public String longestCommonPrefix(String[] strs) {
+        int n = strs.length;
+        String prefix = strs[0];
+
+        for (int i = 1; i < n; i++) {
+            String s1 = strs[i];
+            prefix = getLongestPrefix(s1, prefix);
+        }
+
+        return prefix;
+    }
+
+    private String getLongestPrefix(String s1, String s2) {
+        int i = 0;
+
+        while (i < s1.length() && i < s2.length()) {
+            if (s1.charAt(i) != s2.charAt(i))
+                break;
+
+            i++;
+        }
+
+        return s1.substring(0, i);
+    }
+}
