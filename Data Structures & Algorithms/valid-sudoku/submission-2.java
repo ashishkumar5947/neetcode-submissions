20f62f8c471e2a0c62a05d4ab31472c89ccd50a8
Ashish Kumar <47529525+ashishkumar5947@users.noreply.github.com>
2026-08-19 00:22:24 +0530

    Add: valid-sudoku - submission-2

---
+++ Data Structures & Algorithms/valid-sudoku/submission-2.java
@@ -0,0 +1,76 @@
+class Solution {
+    // public boolean isValidSudoku(char[][] board) {
+    //     int m = board.length, n = board[0].length;
+
+    //     for (int row = 0; row < m; row++) {
+    //         for (int col = 0; col < n; col++) {
+    //             if (board[row][col] == '.')
+    //                 continue;
+
+    //             if (!isValidNumber(board, row, col, m, n)) {
+    //                 return false;
+    //             }
+    //         }
+    //     }
+
+    //     return true;
+    // }
+
+    // private boolean isValidNumber(char[][] board, int i, int j, int m, int n) {
+    //     int num = board[i][j];
+
+    //     for (int k = 0; k < m; k++) {
+    //         if (k == i)
+    //             continue;
+    //         if (board[k][j] == num)
+    //             return false;
+    //     }
+
+    //     for (int k = 0; k < n; k++) {
+    //         if (k == j)
+    //             continue;
+    //         if (board[i][k] == num)
+    //             return false;
+    //     }
+
+    //     int boxRowStart = (i / 3) * 3;
+    //     int boxColStart = (j / 3) * 3;
+
+    //     for (int boxRow = boxRowStart; boxRow < boxRowStart + 3; boxRow++) {
+    //         for (int boxCol = boxColStart; boxCol < boxColStart + 3; boxCol++) {
+    //             if (boxRow == i && boxCol == j)
+    //                 continue;
+    //             if (board[boxRow][boxCol] == num)
+    //                 return false;
+    //         }
+    //     }
+
+    //     return true;
+    // }
+
+    // OPTIMAL
+    public boolean isValidSudoku(char[][] board) {
+        boolean[][] rows = new boolean[9][9];
+        boolean[][] cols = new boolean[9][9];
+        boolean[][] boxes = new boolean[9][9];
+
+        for (int row = 0; row < 9; row++) {
+            for (int col = 0; col < 9; col++) {
+                if (board[row][col] == '.')
+                    continue;
+
+                int num = board[row][col] - '1';
+                int box = (row / 3) * 3 + (col / 3);
+
+                if (rows[row][num] || cols[col][num] || boxes[box][num])
+                    return false;
+
+                rows[row][num] = true;
+                cols[col][num] = true;
+                boxes[box][num] = true;
+            }
+        }
+
+        return true;
+    }
+}
